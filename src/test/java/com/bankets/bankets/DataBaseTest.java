435c45a7cedbd9e764b6809e908d3d942473f8dd
Herman <geragets@gmail.com>
2023-11-26 19:48:23 +0300

    Data Base Tests added

--- src/test/java/com/bankets/bankets/DataBaseTest.java
+++ src/test/java/com/bankets/bankets/DataBaseTest.java
@@ -1,23 +1,22 @@
 package com.bankets.bankets;
-import com.bankets.bankets.controllers.MainController;
+
+import com.bankets.bankets.models.*;
 import com.bankets.bankets.repo.PersonRepository;
 import com.bankets.bankets.repo.RolesRepository;
 import com.bankets.bankets.repo.UsersRepository;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.junit4.SpringRunner;
-import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
-import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+import java.util.Iterator;
 
 import static org.aspectj.bridge.MessageUtil.fail;
+import static org.junit.Assert.assertEquals;
 
-
+@SpringBootTest
+@RunWith(SpringRunner.class)
 public class DataBaseTest {
 
     @Autowired
@@ -29,10 +28,17 @@ public class DataBaseTest {
     @Autowired
     private PersonRepository personRepository;
 
+
     @Test
-    public void testMainPageStatus() throws Exception {
+    public void addToDatabase() throws Exception {
         try{
-            //БД
+            Person person = new Person("+375 (29) 123-45-67", "email@mail.ru ", "Ivan Ivanov");
+            personRepository.save(person);
+
+            Long role_id = rolesRepository.findByPosition("Пользователь").orElseThrow().getRole_id();
+
+            Users users = new Users(role_id,person.getPerson_id(), "IvanIvanov", "123qwerty123");
+            usersRepository.save(users);
         }
         catch (Exception e){
             e.printStackTrace();
@@ -40,4 +46,50 @@ public class DataBaseTest {
         }
     }
 
+
+    @Test
+    public void changeUserRecord() throws Exception {
+        Users users = usersRepository.findByLogin("IvanIvanov").orElseThrow();
+        users.setLogin("IvanIvanov");
+        users.setPassword("newPassword");
+        users.setRole_id(2L);
+
+        Person person = personRepository.findById(users.getPerson_id()).orElseThrow();
+        person.setName("new Name");
+        person.setEmail("new Email");
+        person.setPhone("new Phone");
+        personRepository.save(person);
+
+        usersRepository.save(users);
+    }
+
+    @Test
+    public void readFromDatabase() throws Exception {
+        Users user = usersRepository.findByLogin("IvanIvanov").orElseThrow();
+        assertEquals("IvanIvanov", user.getLogin());
+        assertEquals("newPassword", user.getPassword());
+
+        Person person = personRepository.findById(user.getPerson_id()).orElseThrow();
+        assertEquals("new Name", person.getName());
+        assertEquals("new Email", person.getEmail());
+        assertEquals("new Phone", person.getPhone());
+
+    }
+
+    @Test
+    public void deleteFromDatabase() throws Exception {
+        try{
+            Users user = usersRepository.findByLogin("IvanIvanov").orElseThrow();
+            long person_id = user.getPerson_id();
+
+            usersRepository.delete(user);
+
+            Person person = personRepository.findById(person_id).orElseThrow();
+            personRepository.delete(person);
+        }
+        catch (Exception e){
+            e.printStackTrace();
+            fail("Тест провален");
+        }
+    }
 }
